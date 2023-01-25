4cf3c17a300c7a44a2d3bd090164c0dccb995796
Ryan King <ryan.king2016@loretomilford.net>
2023-01-25 04:56:10 +0000

    Add files via upload

---
+++ Fahrenheit.java
@@ -0,0 +1,16 @@
+package week31029;
+
+public class Fahrenheit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double celsius = 20;
+		
+		double fahrenheit = (((float)9/5) * celsius) + 32;
+		
+		System.out.printf("The Fahrenheit equivalent of " + celsius + " degrees Celsius is " + "%.2f", fahrenheit);
+
+	}
+
+}
