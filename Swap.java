e9cd59f64d0434b2f5dafb36ab6d0bfab8b1e7ba
Ryan King <ryan.king2016@loretomilford.net>
2023-01-25 04:57:32 +0000

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,21 @@
+package week31029;
+
+public class Swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int first = 3;
+		int second = 4;
+		System.out.println("First variable: "+first+" Second variable: "+second);
+		first = first + second;
+		second = first - second;
+		first = first - second;
+		System.out.println("First variable: "+first+" Second variable: " + second);
+		
+		
+	}
+
+}
+
+
